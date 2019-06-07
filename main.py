7eaa58812a43fb473b458ae42530bbb2a79684db
Siki <ndimitrijevic16@raf.rs>
2019-06-07 12:03:01 +0200

    Valjda sve

--- main.py
+++ main.py
@@ -22,7 +22,69 @@ class_names = ['Majica', 'Pantalone', 'Duks', 'Haljina', 'Kaput', 'Sandale', 'Ko
 
 # TODO
 solution = img.copy()
-print(solution)
+
+# Donja i gornja granica za cv da prepozna shape
+donja = np.array([0,0,0])
+gornja = np.array([220,220,220])
+
+# Maska za granice
+mask = cv2.inRange(solution, donja, gornja)
+#ret, thresh = cv2.threshold(solution, 127, 255, cv2.THRESH_BINARY)
+
+# Primena maske na findContours, vraca shapove 
+contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# dobri - dobri shapovi, za_predikcije - slike koje cemo da trpamo u model, xy - x i y za pisanje teksta 
+dobri = []
+za_predikcije = []
+xy = []
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# kernel_sharpen = np.asarray([
+#   [0, -1, 0],
+#   [-1, 5, -1],
+#   [0, -1, 0]
+# ])
+
+# print(contours)
+
+# Odvajamo dobre shapove
+for contour in contours:
+    if contour.shape[0] >= 25:
+        dobri.append(contour)
+
+# Nalazimo pravougaonik u kome se nalazi shape, njega inverzujemo jer model radi sa belim shapovima i crnom pozadinom 
+# Resize na 28,28 gde joj je onda shape (28,28,3), nama treba (28, 28, 1) i zato ga prebacujemo u greyscale i dodajemo jednu dimenziju
+for contour in dobri:
+    (x,y,w,h) = cv2.boundingRect(contour)
+    xy.append((x, y))
+    obl = solution[y:y+h, x:x+w]
+    obl = (255-obl)
+    # obl = cv2.filter2D(obl, -1, kernel_sharpen)
+    resized = cv2.resize(obl, (28, 28))
+    resized = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    resized = np.expand_dims(resized, axis=2)
+    # resized = np.reshape(resized, (28, 28, 1))
+    za_predikcije.append(resized)
+    cv2.rectangle(solution, (x,y), (x+w,y+h), (255,0,0), 2)
+    
+    # solution = cv2.putText(solution, a, (x, y), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+
+# Nalazimo predikcije
+za_predikcije = np.array(za_predikcije)
+predictions = model.predict_classes(za_predikcije, verbose=1)
+print(predictions)
+
+# Stavljamo imena klasa na njihova mesta
+for oba, idx in zip(xy, predictions):
+    name = class_names[idx]
+    solution = cv2.putText(solution, name, (oba[0]-15, oba[1]-5), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+cv2.imshow('img', solution)
+cv2.waitKey(0)
+
+
 #################################################################################
 
 # Cuvamo resenje u izlazni fajl
--- model.py
+++ model.py
@@ -20,10 +20,13 @@ y_test = keras.utils.to_categorical(y_test)
 # U ovoj sekciji implementirati Keras neuralnu mrezu koja postize tacnost barem
 # 85% na test skupu. Ne menjati fajl van ove sekcije.
 
+# Delimo sve sa 255 jer hocemo inputi da budu od 0 do 1
 x_train = x_train / 255
 x_test = x_test / 255
 
 # Za obican model ovo ne treba
+# Za convolucionu treba nam dodatna dimenzija za dubino
+# S ozirom da su podaci greyscale treba nam samo 1
 x_train = x_train.reshape(10000, 28, 28, 1)
 x_test = x_test.reshape(10000, 28, 28, 1)
 
@@ -31,26 +34,25 @@ x_test = x_test.reshape(10000, 28, 28, 1)
 
 # model = keras.Sequential([
 #     keras.layers.Flatten(input_shape=(28, 28)),
-#     keras.layers.Dense(128, activation=tf.nn.relu),
-#     keras.layers.Dense(128, activation=tf.nn.sigmoid),
-#     keras.layers.Dense(10, activation=tf.nn.softmax)
+#     keras.layers.Dense(128, activation='relu'),
+#     keras.layers.Dense(128, activation='sigmoid'),
+#     keras.layers.Dense(10, activation='softmax')
 # ])
 
-# Model sa jednim konvolucionim slojem
+# Model sa jednim konvolucionim slojem, mislim da je ovaj top za sad
 # Duze traje, bolja tacnost
-
 model = keras.Sequential([
-    keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
-    keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    keras.layers.Dropout(0.2),
+    keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),        # Konvolucioni sloj sa 32 filtera i kernelom (3, 3)
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),                                                    # Smanjujemo dimenzije
+    keras.layers.Dropout(0.2),                                                                      # Ignorisemo neke neurone da ne overfitujemo
 
-    # keras.layers.Conv2D(32, kernel_size=3, activation='relu'),
-    keras.layers.Flatten(),
-    keras.layers.Dense(128, activation='relu'),
+    # keras.layers.Conv2D(32, kernel_size=3, activation='relu'),    
+    keras.layers.Flatten(),                                                                         # Flattenujemo da bi mogli tensore dalje normalno da koristimo
+    keras.layers.Dense(128, activation='relu'),                                                     # 1 obican i 1 output sloj
     keras.layers.Dense(10, activation='softmax')
 ])
 
-# Model za 3 konvoluciona sloja, meh
+# Model sa 3 konvoluciona sloja, meh
 
 # model = keras.Sequential([
 #     keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
@@ -61,9 +63,11 @@ model = keras.Sequential([
 #     keras.layers.Dropout(0.25),
 #     keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
 #     keras.layers.BatchNormalization(),
+#     keras.layers.MaxPooling2D(pool_size=(2, 2)),
 #     keras.layers.Dropout(0.25),
 #     keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
 #     keras.layers.BatchNormalization(),
+#     keras.layers.MaxPooling2D(pool_size=(2, 2)),
 #     keras.layers.Dropout(0.25),
 
 #     keras.layers.Flatten(),
@@ -77,6 +81,20 @@ model = keras.Sequential([
 
 #     keras.layers.Dense(10, activation='softmax')
 # ])
+ 
+# Model sa 2 konv, oko 88.5% 
+
+# model = keras.Sequential([
+#     keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+#     keras.layers.MaxPooling2D(pool_size=(2, 2)),
+#     keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+#     keras.layers.MaxPooling2D(pool_size=(2, 2)),
+#     keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
+
+#     keras.layers.Flatten(),
+#     keras.layers.Dense(128, activation='relu'),
+#     keras.layers.Dense(10, activation='softmax')
+# ])
 
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -88,4 +106,4 @@ print(f'Preciznost {test_acc}')
 #################################################################################
 
 # Cuvanje istreniranog modela u fajl
-#model.save('fashion.h5')
+model.save('fashion.h5')
